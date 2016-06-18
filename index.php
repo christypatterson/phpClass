0bf00f1bfa23ef984371da3a2f2adacb9774d174
Christy Patterson <christypatterson@users.noreply.github.com>
2016-06-18 16:35:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,7 @@
 <?php
 
-  $myArray=array("pizza", "chocolate", "juice");
+  $number=1;
 
-  print_r($myArray);
-
-  echo $myArray[5];
-
-  echo "<br /><br />";
-
-  $anotherArray[0]="pretzel";
-  $anotherArray[1]="bagel";
-
-  print_r($anotherArray);
-
-  echo "<br /><br/>";
-
-  $thirdArray=array(
-
-       "France" => "French",
-       "USA" => "English",
-       "Germany" => "German"
-
-  );
-
-  print_r($thirdArray);
-
-  $anotherArray[]="salad";
-
-  echo "<br /><br />";
-
-  print_r($anotherArray);
-       
-  echo "<br /><br />";
-
-  unset($thirdArray["Germany"]);
-
-  print_r($thirdArray);
-
-  echo "<br /><br />";
-
-  $name="Shem";
-
-  unset($name);
-
-  echo $name;
+  echo $number;
 
 ?>
