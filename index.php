22f9018db85acc1b234642fead4f0582cd74be1d
Christy Patterson <christypatterson@users.noreply.github.com>
2016-06-18 12:38:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
   print_r($myArray);
 
   echo $myArray[5];
-  
-  echo "I'm still going"
+
+  echo "<br /><br />";
+
+  $anotherArray[0]="pretzel";
+  $anotherArray[1]="bagel";
+
+  print_r($anotherArray);
 
 ?>
