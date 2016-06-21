00c78ec7de439e4675b74644241d5d54eeb3e894
Christy Patterson <christypatterson@users.noreply.github.com>
2016-06-20 17:36:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
+ $array=array("cat", "dog", "turtle", "kangaroo");
+
+  foreach ($array as $key => $value)  {
+
+    echo "Key:$key Value:$value <br />";
+
+  }
+  
+ /*
+
   for ($i=500; $i>=100; $i=$i-5)  {
 
     echo $i."<br />";
 
   }
+
+  */
 
 ?>
