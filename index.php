38936c69d2176bd84ca48352229364b0b5833a22
Christy Patterson <christypatterson@users.noreply.github.com>
2016-06-20 18:46:47 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
-<?php
-
-  $i=0;
-
-  $array=array("apple", "banana", "grape");
-
-  while ($array[$i]) {
-
-    echo "Key: $i Value: $array[$i]"."<br />";
-
-    $i++;
-
-
-  }
-
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Mail Test</title>
+</head>
+<body>
+<div>
+ <?php 
+$emailTo="christy.patterson@gmail.com";
+ $subject="I hope this works";
+ $body="I think this is great";
+ $headers="From: shem.patterson@gmail.com";
+if (mail($emailTo, $subject, $body, $headers)) {
+ echo "Mail set successfully!";
+ } else {
+ echo "Mail not sent!";
+ }
 ?>
+</div>
+</body>
+</html>
