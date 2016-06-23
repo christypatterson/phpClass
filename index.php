fef223c097ac969a9d8cb7ff739edead049e72b6
Christy Patterson <christypatterson@users.noreply.github.com>
2016-06-23 15:41:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,22 @@
  <title>My Webpage</title>
 </head>
 
-<!DOCTYPE html>
-<html>
-<head>
- <meta charset="UTF-8" />
- <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
- <meta name="viewport" content="width=device-width, initial-scale-=1" />
- <title>My Webpage</title>
-</head>
-
 <body>
 <div>
 
   <?php
 
-    if ($_GET["language"]) {
+    if ($_GET["submit"]) {
 
-     echo "Your chosen language is: ".($_GET['language']);
+      if ($_GET["language"]) {
+
+       echo "Your chosen language is: ".($_GET['language']);
+
+     }  else  {
+
+       echo "Please enter your language.";
+
+     }
 
   }
 
