a895ec94ac18b79bd5a7562a214f7ee2f7f7f099
Christy Patterson <christypatterson@users.noreply.github.com>
2016-06-24 11:28:50 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,51 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8" />
+ <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale-=1" />
+ <title>My Webpage</title>
+</head>
 
- $array=array("cat", "dog", "turtle", "kangaroo");
+<body>
+<div>
 
-  foreach ($array as $key => $value)  {
+  <?php
+    if ($_GET["submit"]) {
+      if ($_GET["language"]) {
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8" />
+ <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale-=1" />
+ <title>My Webpage</title>
+</head>
+<body>
+<div>
+  <?php
+    $names=array("Bob", "Shem", "Tom") ;
+      if ($_POST["submit"]) {
+        if ($_POST["name"]) {
+           foreach ($names as $name)  {
+         
+              if ($_POST["name"]==$name)  {
+           echo "Hello again! ".$name;
+           $true=1;
+          }  
+        }
+       if (!$true) echo "I don't know you,  ".$_POST['name'];
+        }
+     }
+  ?>
 
-    echo "Key:$key Value:$value <br />";
 
-  }
-  
- /*
+  <form method="post">
+    <label for="lname">Name</label>
+    <input type="text" name="name" />
+    <input type="submit" name="submit" value="Submit your name" />
+  </form>
 
-  for ($i=500; $i>=100; $i=$i-5)  {
-
-    echo $i."<br />";
-
-  }
-
-  */
-
-?>
+</div>
+</body>
+</html>
